86f3e71389d4896ecf43bade8c6050f71ed4e04c
Sven Schmit <schmit@stanford.edu>
2014-12-15 14:07:14 -0800

    added description of agents

--- agent.py
+++ agent.py
@@ -7,17 +7,27 @@ class Agent:
 
 
 class RandomAgent(Agent):
+    '''
+    picks a valid move at action
+    '''
     def get_action(self, available_actions):
         random_action = random.choice(available_actions.keys())
         return available_actions[random_action]
 
+
 class BestScoreAgent(Agent):
+    '''
+    picks move that yields the highest possible score on that turn
+    '''
     def get_action(self, available_actions):
         best_move = sorted([(-score, move) for move, (board, score) in available_actions.iteritems()])[0][1]
         return available_actions[best_move]
 
 
 class Human(Agent):
+    '''
+    queries user to pick move
+    '''
     def __init__(self):
         self.move_d = {'l': 'left', 'r': 'right', 'u': 'up', 'd': 'down'}
 
